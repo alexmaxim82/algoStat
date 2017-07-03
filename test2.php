ae2f82d3d6cf818ad7a6f1364c2c35c8f82b037c
alexm <petitpapillon111>
2017-07-03 17:07:00 +0200

    test 02

--- test2.php
+++ test2.php
@@ -5,6 +5,27 @@
  * Date: 03/07/2017
  * Time: 16:05
  */
-echo test2;
 
+algoInsertion($array)
+{
+    $i = 0;
+    $size = count($array);
+    while ($i < $size)
+    {
+        $cpy = $array[$i];
+        $j = 0;
+        while ($j < $i)
+        {
+            $compared = $array[$j];
+            if ($cpy < $compared)
+            {
+                $array[$j] = $cpy;
+                $cpy = $compared;
+            }
+            $array[$i] = $cpy;
+            $j++;
+        }
+        $i++;
+    }
+}
 ?>
