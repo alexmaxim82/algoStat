fec040f80bc2fb9aa941155d288903af710182ac
alexm <petitpapillon111>
2017-07-18 08:56:03 +0200

    test 02

---
+++ algoMerge.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexm
+ * Date: 17/07/2017
+ * Time: 17:54
+ */
+https://fr.m.wikibooks.org/wiki/Impl%C3%A9mentation_d%27algorithmes_classiques/Algorithmes_de_tri/Tri_fusion
