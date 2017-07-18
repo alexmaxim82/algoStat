45b9d2e750006f66ab8512e6be3b2b6553d29e4a
alexm <petitpapillon111>
2017-07-18 13:38:01 +0200

    test 02

--- algoMerge.php
+++ algoMerge.php
@@ -6,3 +6,18 @@
  * Time: 17:54
  */
 https://fr.m.wikibooks.org/wiki/Impl%C3%A9mentation_d%27algorithmes_classiques/Algorithmes_de_tri/Tri_fusion
+
+function algoMerge(&$tab){
+    if(count($tab)) <= 1) return;
+    else{
+        $tab1 = array();
+        $tab2 = array();
+
+        for($i =0; $i < count($tab); $i++){
+            if ($i <(count()))
+        }
+
+    }
+
+
+}
